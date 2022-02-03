dda7623fc4c8296d2bc038e966d89915c8cce2e7
Robert Brennan <accounts@rbren.io>
2022-02-03 13:05:41 -0500

    Update download-kubectl.sh

--- stable/insights-agent/download-kubectl.sh
+++ stable/insights-agent/download-kubectl.sh
@@ -11,8 +11,10 @@ default_kubectl_version='v1.19.6'
 echo "Downloading jq . . ."
 curl -sSLo jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 && chmod +x jq
 echo "Getting the Kubernetes version from the API. . ."
-kube_version=$(curl -ksS https://kubernetes.default.svc/version?timeout=32s | jq -r .gitVersion | cut -d- -f1)
-echo "Found Kubernetes version $kube_version"
+kube_version=$(curl -fksS https://kubernetes.default.svc/version?timeout=32s)
+echo "Found Kubernetes version info: $kube_version"
+kube_version=$(echo "${kube_version}" | jq -r .gitVersion | cut -d- -f1)
+echo "Extracted gitVersion: $kube_version"
 kube_minor_version=$(echo $kube_version |cut -d. -f2)
 echo "Minor version is ${kube_minor_version}"
 if [ "x${kube_version}" == "x" ] ; then
